08671c59a6aba29db49231005069748be3f3f4a6
KaimNotark <KaimNotark@yandex.ru>
2019-06-16 10:37:08 +0800

    fix mobile rotate

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -4713,7 +4713,7 @@ window.onscroll = function () {
   // console.log(scrollYPos);
 
   if (481 <= widthWindow && widthWindow <= 710) {
-    minYhr = 2870;
+    minYhr = 2970;
   } else {
     minYhr = 3300;
   }
@@ -4934,7 +4934,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58103" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58738" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
